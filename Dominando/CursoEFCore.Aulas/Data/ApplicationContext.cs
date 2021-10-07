fa9374b1ddcc47eeae8c849a70bc0a5fcf887939
gustavobruder <gugabruder@gmail.com>
2021-10-07 20:28:27 -0300

    Adicionado setup do projeto do curso 'Dominando Entity Framework Core'

---
+++ Dominando/CursoEFCore.Aulas/Data/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CursoEFCore.Aulas.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace CursoEFCore.Aulas.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string server = "localhost";
+            const string port = "1433";
+            const string user = "SA";
+            const string password = "Curso#Entity";
+            const string database = "CursoEFCore";
+            var connectionString = $"Server={server}, {port};Initial Catalog={database};User ID={user};Password={password}";
+
+            optionsBuilder
+                .UseSqlServer(connectionString)
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+    }
+}
---
+++ Dominando/CursoEFCore.Aulas/Domain/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿namespace CursoEFCore.Aulas.Domain
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+
+        public int DepartamentoId { get; set; }
+        public Departamento Departamento { get; set; }
+    }
+}
